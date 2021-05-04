8a6fa1f0ca56e03dc6bf31f088e0ca2d7861ebf6
Aidan McAra <51344785+aidansbhs@users.noreply.github.com>
2021-05-04 15:09:18 +1200

    Add/minus button
    More progress on products page

--- code.js
+++ code.js
@@ -25,50 +25,44 @@ var products4 = [
 function itemSelect() {
     for (let ii = 1; ii <= 1; ii++) {
         for (let i = 0; i < products.length; i++) {
-            let items = '<div class="temp"><img class="img" src="' + products[i][0] + '"> <h3>' + products[i][1] + '</h3> <h5>' + products[i][3] + '</h5> <h3>' + products[i][2] + '</h3></div>';
+            let items = '<div class="temp"><img class="img" src="' + products[i][0] + '"> <h3>' + products[i][1] + '</h3> <h5>' + products[i][3] + '</h5> <h3>$' + products[i][2] + '</h3></div>';
             document.getElementById('item' + ii).innerHTML += items;
         }
     }
 
     for (let ii = 2; ii <= 2; ii++) {
         for (let i = 0; i < products2.length; i++) {
-            let items2 = '<div class="temp"><img class="img" src="' + products2[i][0] + '"> <h3>' + products2[i][1] + '</h3> <h5>' + products2[i][3] + '</h5> <h3>' + products2[i][2] + '</h3></div>';
+            let items2 = '<div class="temp"><img class="img" src="' + products2[i][0] + '"> <h3>' + products2[i][1] + '</h3> <h5>' + products2[i][3] + '</h5> <h3>$' + products2[i][2] + '</h3></div>';
             document.getElementById('item' + ii).innerHTML += items2;
         }
     }
 
     for (let ii = 3; ii <= 3; ii++) {
         for (let i = 0; i < products3.length; i++) {
-            let items3 = '<div class="temp"><img class="img" src="' + products3[i][0] + '"> <h3>' + products3[i][1] + '</h3> <h5>' + products3[i][3] + '</h5> <h3>' + products3[i][2] + '</h3></div>';
+            let items3 = '<div class="temp"><img class="img" src="' + products3[i][0] + '"> <h3>' + products3[i][1] + '</h3> <h5>' + products3[i][3] + '</h5> <h3>$' + products3[i][2] + '</h3></div>';
             document.getElementById('item' + ii).innerHTML += items3;
         }
     }
 
     for (let ii = 4; ii <= 4; ii++) {
         for (let i = 0; i < products4.length; i++) {
-            let items4 = '<div class="temp"><img class="img" src="' + products4[i][0] + '"> <h3>' + products4[i][1] + '</h3> <h5>' + products4[i][3] + '</h5> <h3>' + products4[i][2] + '</h3></div>';
+            let items4 = '<div class="temp"><img class="img" src="' + products4[i][0] + '"> <h3>' + products4[i][1] + '</h3> <h5>' + products4[i][3] + '</h5> <h3>$' + products4[i][2] + '</h3></div>';
             document.getElementById('item' + ii).innerHTML += items4;
         }
     }
 } //end of itemSelect
 
-//function changeNum(){
-//    document..getElementById('wow').onclick = function(){
-//        addNum(0);
-//    }
-//}
-
 function add(num) {
-    var text = document.getElementById('wow' + num).value;
+    var text = document.getElementById('number' + num).value;
     text = Number(text);
-    document.getElementById('wow' + num).value = text += 1;
+    document.getElementById('number' + num).value = text += 1;
 } //end of add
 
 function minus(num) {
-    var text = document.getElementById('wow' + num).value;
+    var text = document.getElementById('number' + num).value;
     text = Number(text);
     if (text >= 1) {
-        document.getElementById('wow' + num).value = text -= 1;
+        document.getElementById('number' + num).value = text -= 1;
     }
 } //end of minus
 
