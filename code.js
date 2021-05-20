31d0c111fba53e14f1eb713b2df6dbeab2fe4bcf
Aidan McAra <51344785+aidansbhs@users.noreply.github.com>
2021-05-20 13:31:40 +1200

    Finished product page
    Fixed add/minus buttons and every other aspect is now running correctly

--- code.js
+++ code.js
@@ -1,12 +1,18 @@
 var products = [
-  ['imgs/Products/Product%201.png', 'Fancy Chair', 2500, 'description1'],
-  ['imgs/Products/Product%201.png', 'Not Fancy Chair', 3500, 'description2'],
-  ['imgs/Products/Product%201.png', 'Random Chair', 1000, 'description3'],
-  ['imgs/Products/Product%201.png', 'Epic Chair', 2000, 'description4']
+  ['imgs/Products/Product%201.png', 'Fancy Chair', 200, 'description'],
+  ['imgs/Products/Product%201.png', 'Not Fancy Chair', 90, 'description'],
+  ['imgs/Products/Product%201.png', 'Random Chair', 75, 'description'],
+  ['imgs/Products/Product%201.png', 'Epic Chair', 500, 'description'],
+  ['imgs/Products/Product%201.png', 'Sunday Afternoon Chair', 25, 'description'],
+  ['imgs/Products/Product%201.png', 'Nice Chair', 100, 'description']
 ];
 
 var cartItems = [];
 var totalItems = 0;
+var empty = false;
+var plus = '<div class="increase">' + '+' + '</div>';
+var minus = '<div class="decrease">' + '-' + '</div>';
+var cartAdd = '<div class="cartAdd">' + 'Add To Cart' + '</div>';
 
 function loadProducts() { //loading products on the product page
 
@@ -20,10 +26,9 @@ function loadProducts() { //loading products on the product page
     var header = document.createElement('h2');
     var price = document.createElement('h2');
     var desc = document.createElement('p');
-
     var increase = document.createElement('button');
-    var decrease = document.createElement('button');
     var box = document.createElement('input');
+    var decrease = document.createElement('button');
     var add = document.createElement('button');
 
     //appendchild makes all these elements childs of the list or goes under it
@@ -32,10 +37,9 @@ function loadProducts() { //loading products on the product page
     ele.appendChild(header);
     ele.appendChild(price);
     ele.appendChild(desc);
-
     ele.appendChild(increase);
-    ele.appendChild(decrease);
     ele.appendChild(box);
+    ele.appendChild(decrease);
     ele.appendChild(add);
 
     //sets these elements content to the html page
@@ -43,31 +47,45 @@ function loadProducts() { //loading products on the product page
     header.innerHTML = products[i][1];
     price.innerHTML = '$' + products[i][2];
     desc.innerHTML = products[i][3];
-
-    increase.innerHTML = "+";
-    decrease.innerHTML = "-";
+    increase.innerHTML = plus;
+    decrease.innerHTML = minus;
     box.type = 'number';
-    add.innerHTML = 'Add To Cart';
-
+    add.innerHTML = cartAdd;
     box.setAttribute('id', 'input' + i);
     box.value = 1;
+
+    increase.dataset.cartIndex = i;
+    increase.addEventListener('click', increasing, false);
+
+    decrease.dataset.cartIndex = i;
+    decrease.addEventListener('click', decreasing, false);
 
     add.dataset.cartIndex = i;
     add.addEventListener('click', adding, false);
   }
 } //end of loadProducts
 
+function increasing(num) {
+  const NUM = event.currentTarget.dataset.cartIndex;
+  var inputBox = document.getElementById('input' + NUM);
+  inputBox.value++;
+} //end of increasing
+
+function decreasing(num) {
+  const NUM = event.currentTarget.dataset.cartIndex;
+  var inputBox = document.getElementById('input' + NUM);
+  if (inputBox >= 1) { //stops it from going negative
+    inputBox.value--;
+  }
+}
+
 function adding(event) { //function for the button to add products to cart
   const NUM = event.currentTarget.dataset.cartIndex;
-  cartItems.push([products[NUM]]);
+  cartItems.push([products[NUM]]); //pushes the products as the number dataset into the cart
   cartItems[cartItems.length - 1][1] = Number(document.getElementById('input' + NUM).value);
 
   updateCart();
 } //end of adding
-
-function increase(){
-
-}
 
 function updateCart() {
   var itemCounter = document.getElementById('itemCount');
@@ -82,12 +100,10 @@ function updateCart() {
 } //end of updateCart
 
 function loadCart() { // loading products on cart page
-
   var main = document.getElementById('cartProducts'); //where these elements will go on the cart html page
 
   var data = sessionStorage.getItem('cartItems');
   data = JSON.parse(data);
-
   cartItems = data;
 
   updateCart();
@@ -109,26 +125,24 @@ function loadCart() { // loading products on cart page
     ele.appendChild(pic);
     ele.appendChild(header);
     ele.appendChild(price);
-    ele.appendChild(subtract);
     ele.appendChild(amount);
     ele.appendChild(subtotal);
+    ele.appendChild(subtract);
     ele.appendChild(grandTotal);
 
     //sets these elements content to the html page
     pic.src = cartItems[i][0][0];
-    header.innerHTML = cartItems[i][0][1];
-    price.innerHTML = '$' + cartItems[i][0][2];
-    amount.innerHTML = cartItems[i][1];
-    subtotal.innerHTML = '$' + cartItems[i][1] * cartItems[i][0][2];
+    header.innerHTML = '<div class="name">' + cartItems[i][0][1] + '</div>';
+    price.innerHTML = '<div class="price">' + '$' + cartItems[i][0][2] + '</div>';
+    amount.innerHTML = '<div class="amount">' + cartItems[i][1] + '</div>';
+    subtotal.innerHTML = '<div class="subtotal">' + '$' + cartItems[i][1] * cartItems[i][0][2] + '</div>';
     // grandTotal.innerHTML = '$' + cartItems[0][2] * cartItems[i][2];
 
-    subtract.innerHTML = 'Remove';
+    subtract.innerHTML = '<div class="delete">' + 'Remove' + '</div>';
     subtract.dataset.cartIndex = i;
     subtract.addEventListener('click', deleteItems, false);
   }
 } //end of loadCart
-
-var empty = false;
 
 function deleteItems() { //function for removing the items in the cart
   const NUM = event.currentTarget.dataset.cartIndex;
@@ -136,12 +150,13 @@ function deleteItems() { //function for removing the items in the cart
   cartItems = cartItems.filter(item => item !== undefined);
   updateCart();
   loadCart();
-  window.location.reload(true);
+  // window.location.reload(true);
 
-  if(cartItems == 0){
+  if (cartItems.length == 0) {
+    console.log(empty);
     empty = true;
   }
-  if(empty == true){
+  if (empty == true) {
     var main = document.getElementById('cartProducts');
     var text = document.createElement('h2');
     main.appendChild(text);
